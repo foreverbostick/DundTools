f8fa9e906fdabc2a1b748b2cbd9c13f73322a489
foreverbostick <brian.bostick@protonmail.com>
2024-01-18 14:37:59 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A collection of Dungeons & Dragons tools to use during game sessions.
+
+Very *very* much a work in progress.
---
+++ diceroll/diceroll.py
@@ -0,0 +1,10 @@
+from random import randint
+import sys, math
+
+# Make a little title bar thing
+print('''
+      -=-=-=-=-=-=-=-=-=-=-=-=-=-
+      DundTools Dice Roller v0.01
+      -=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+      ''')
