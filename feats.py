de2b97a68e36c8030b3e42ab86e3a8e5903a9f8e
foreverbostick <brian.bostick@protonmail.com>
2024-02-05 13:18:54 -0500

    Made list of feats

---
+++ feats.py
@@ -0,0 +1,19 @@
+#
+# Feats
+#
+
+import stats as s
+
+
+featList = ["Actor","Alert","Athlete","Charger","Crossbow Expert",
+            "Defensive Duelist","Dual Wielder", "Dungeon Delver","Durable",
+            "Elemental Adept","Grappler","Great Weapon Master","Healer",
+            "Heavily Armored","Heavy Armor Master","Inspiring Leader",
+            "Keen Mind","Lightly Armored","Linguist","Lucky","Mage Slayer",
+            "Magic Initiate","Martial Adept","Medium Armor Master","Mobile",
+            "Moderately Armored","Mounted Combatant","Observant",
+            "Polearm Master","Resilient","Ritual Caster","Savage Attacker",
+            "Sentinel","Sharpshooter","Shield Master","Skilled","Skulker",
+            "Spell Sniper","Tavern Brawler","Tough","War Caster",
+            "Weapon Master"]
+
